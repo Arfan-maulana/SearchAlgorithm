2770cc56f0ed973c89a12253cd35a928dfca50a3
Muhammad Arfan <muhammad.arfan.ft21@mail.umy.ac.id>
2022-10-24 16:26:47 +0700

    accpect array element

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,16 +20,36 @@ namespace SearchAlgorithm
         {
             while (true)
             {
-                Console.Write("Enter the number element ")
+                Console.Write("Enter the number elements in the array  ");
+                string s = Console.ReadLine();
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 elemets.\n");
+            }
+            // accpect array element
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine(" Enter array elements ");
+            Console.WriteLine("-----------------------------");
+            for (i = 0; i< n; i++)
+            {
+                Console.Write("<" + (i + 1)+ ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
             }
 
+        }
 
 
 
-
-
+        static void main()
+        {
 
         }
-       
+
+
+
     }
+
 }
