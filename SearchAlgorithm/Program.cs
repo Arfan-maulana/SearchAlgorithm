f66b50b59ab251d825b9e81c3e7a9731660fabba
Muhammad Arfan <muhammad.arfan.ft21@mail.umy.ac.id>
2022-10-24 16:09:16 +0700

    Add project files.

---
+++ SearchAlgorithm/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithm
+{
+     class Program
+    {
+        //Array to be Searched 
+        int[] arr = new int[20];
+        //NUMBER OF EL  ELEMT in the array
+        int n;
+        // get the number of element to store in the array
+        int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number element ")
+            }
+
+
+
+
+
+
+
+        }
+       
+    }
+}
